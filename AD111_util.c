3f73018f3a7d53230f6f4afbdb19ec3eba5fdeb0
Legend Liao <legend_liao@lannerinc.com>
2020-01-13 20:58:40 +0800

    update com port attr. update read/write function.

--- AD111_util.c
+++ AD111_util.c
@@ -23,66 +23,37 @@ struct termios old_tios,SerialPortSettings;
 pthread_t InQueueID;
 pthread_t CheckingThread;
 pthread_mutex_t buf_mut;
-pthread_mutex_t port_mut;
+
 u_int8_t RxBuffer[BUFFER_SIZE];
 
-/*****************/
-//void* _IncomeInQueueThread(void* object);
-//int32_t _OpenLcmPort(char* pLsmPath);
-//void _CloseLcmPort(void);
-//int32_t _SendBufferLength(u_int8_t* buffer, int32_t length);
-//int _ReadBufferLength(u_int8_t* buffer, int32_t length);
-//void _search_LCM_port(void);
 
-
-int set_interface_attribs (int fd, int speed, int parity)
+int set_interface_attribs (int speed)
 {
-       // struct termios SerialPortSettings;
-        //memset (&SerialPortSettings, 0, sizeof SerialPortSettings);
-      //  if (tcgetattr (fd, &SerialPortSettings) != 0)
-       // {
-                //printf("error %d from tcgetattr", errno);
-       //         return -1;
-       // }
 
+tcgetattr(fcom, &SerialPortSettings);
         cfsetospeed (&SerialPortSettings, speed);
         cfsetispeed (&SerialPortSettings, speed);
-		/* 8N1 Mode */
-		SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
-		SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
-		SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
-		SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
+		// 8N1 Mode 
+		SerialPortSettings.c_cflag &= ~PARENB;   // Disables the Parity Enable bit(PARENB),So No Parity   
+		SerialPortSettings.c_cflag &= ~CSTOPB;   // CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit 
+		SerialPortSettings.c_cflag &= ~CSIZE;	 // Clears the mask for setting the data size             
+		SerialPortSettings.c_cflag |=  CS8;      // Set the data bits = 8                                 
 		
-		SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
-		SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
+		SerialPortSettings.c_cflag &= ~CRTSCTS;       // No Hardware flow Control                         
+		SerialPortSettings.c_cflag |= CREAD | CLOCAL; // Enable receiver,Ignore Modem Control lines        
 		
 		
-		SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
-		SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
+		SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          // Disable XON/XOFF flow control both i/p and o/p 
+		SerialPortSettings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);  // Non Cannonical mode                            
 
-		SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
-       /* SerialPortSettings.c_cflag = (SerialPortSettings.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        //SerialPortSettings.c_iflag &= ~IGNBRK;         // disable break processing
-        //SerialPortSettings.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        SerialPortSettings.c_oflag = 0;                // no remapping, no delays
-        SerialPortSettings.c_cc[VMIN]  = 0;            // read doesn't block
-        SerialPortSettings.c_cc[VTIME] = 0;            // 0.5 seconds read timeout
-
-        SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-        SerialPortSettings.c_cflag |= (CLOCAL | CREAD);	// ignore modem controls,
-                                        	// enable reading
-        SerialPortSettings.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        SerialPortSettings.c_cflag |= parity;
-        SerialPortSettings.c_cflag &= ~CSTOPB;
-        SerialPortSettings.c_cflag &= ~CRTSCTS;*/
-printf("c flag = %X\n",SerialPortSettings.c_cflag);
-printf("i flag = %X\n",SerialPortSettings.c_iflag);
-printf("o flag = %X\n",SerialPortSettings.c_oflag);
-printf("l flag = %X\n",SerialPortSettings.c_lflag);
-        if (tcsetattr (fd, TCSANOW, &SerialPortSettings) != 0)
+		SerialPortSettings.c_oflag |= OPOST;//No Output Processing
+		SerialPortSettings.c_oflag |= (ONOCR);//No Output CR
+       SerialPortSettings.c_oflag &= ~(ONLCR | OCRNL | ONLRET);//No Output CR
+//printf("c flag = %X\n",SerialPortSettings.c_cflag);
+//printf("i flag = %X\n",SerialPortSettings.c_iflag);
+//printf("o flag = %X\n",SerialPortSettings.c_oflag);
+//printf("l flag = %X\n",SerialPortSettings.c_lflag);
+        if (tcsetattr (fcom, TCSANOW, &SerialPortSettings) != 0)
         {
                 printf("error %d from tcsetattr", errno);
                 return -1;
@@ -93,30 +64,29 @@ printf("l flag = %X\n",SerialPortSettings.c_lflag);
 
 void* _IncomeInQueueThread(void* object)
 {
-unsigned char tmpBuffer[32];
+unsigned char tmpBuffer[60];
 int xi,n;
 u_int8_t xch;
-//printf("incoming queue\n");
+printf("incoming\n");
 	while(1)
 	{
-printf("XXX n=, \n");
-		n=read(fcom, tmpBuffer, 32);
-printf("receive n=%d, \n", n);
-fflush(stdout);
-		if ( n > 0 && n < 32 ) {
+
+		n=read(fcom, tmpBuffer, 60);
+//printf("receive n=%d, \n", n);
+//fflush(stdout);
+		if ( n > 0 && n < 60 ) {
 //printf("receive n=%d, \n", n);
 			for ( xi=0 ; xi<n ; xi++) {
 					xch = tmpBuffer[xi];
-printf("Rev. 0x%02X \n", xch);
-					pthread_mutex_lock(&buf_mut);	
-//printf("-%s-\n",xch);				
+printf("%c", xch);
+					pthread_mutex_lock(&buf_mut);				
 					*pWritePtr = xch;
 					wRxCounter++;
 					if ( pWritePtr == pRxBufferEnd ) pWritePtr = pRxBufferStart;
 					else				    pWritePtr++;
 					pthread_mutex_unlock(&buf_mut);
 			}
-printf("\n");
+//printf("\n");
 		}
 //		else{
 //			usleep(5000);
@@ -124,34 +94,7 @@ printf("\n");
 	}
 	return NULL;
 }
-/*
-void* _IncomeInQueueThread(void* object)
-{
-unsigned char tmpBuffer[512];
-int xi;
-uint8_t xch;
 
-	while(1)
-	{
-		int n=read(fcom, tmpBuffer, 512);
-		if ( n > 0 && n < 512 ) {
-			pthread_mutex_lock(&buf_mut);
-			for ( xi=0 ; xi<n ; xi++) {
-					xch = tmpBuffer[xi];
-	
-							
-			}
-			pthread_mutex_unlock(&buf_mut);
-		}
-		else {
-			//printf("sleep\n");
-			usleep(5000);	//delay 1ms
-			
-		}
-	}
-	return NULL;
-}
-*/
 /******************************************/
 
 int _SendBufferLength(u_int8_t* buffer, int32_t length)
@@ -208,44 +151,44 @@ u_int8_t *bptr, xch;
 	return iResult;
 }
 
-
-
-/**************************************************
-int _ReadBuffer_LF(int8_t* buffer)
+/**************/
+int _ReadBuffer(int8_t* buffer)
 {
 int iResult = 0;
 int fEnding =0;
 int waitCnt, rxCnt;
 int8_t *bptr, xch;
 
-	//if ( !fIgnOpened_p2 ) return iResult;
+	//if ( !fIgnOpened ) return iResult;
 	rxCnt=0;
 	waitCnt = 0;
 	bptr = buffer;
 	
 	while (!fEnding)
 	{
-		if ( wIgnRxCounter == 0 ) 
+		if ( wRxCounter == 0 ) 
 		{
-			_ign_msdelay(4); //4ms
+			sleep(5);
 			waitCnt++;
 			if ( waitCnt >= 25 )  fEnding = 1;
 		}
 		else {
 			waitCnt = 0;
 			pthread_mutex_lock(&buf_mut);
-			xch = *pIgnReadPtr;
-			if ( xch != 0x0d && xch != 0x0a  && xch != 0x00 ) {
+			xch = *pReadPtr;
+			if ( xch != 0x3e && xch != 0x0a  && xch != 0x00 ) {
 				//maybe ignore all CTRL code 
+//printf("%c",xch);
 				*bptr++ = xch;
 				rxCnt++;
 			}
-			wIgnRxCounter_p2--;
-			if ( pIgnReadPtr_p2 == pIgnRxBufferEnd_p2) pIgnReadPtr_p2 = pIgnRxBufferStart_p2;
-			else				  pIgnReadPtr_p2++;
-			pthread_mutex_unlock(&ign_buf_mut_p2);
+			wRxCounter--;
+			if ( pReadPtr == pRxBufferEnd) pReadPtr = pRxBufferStart;
+			else				  pReadPtr++;
+			pthread_mutex_unlock(&buf_mut);
 			
-			if ( xch == 0x0a || rxCnt >= MAX_STR_LENGTH )  {
+			if ( (xch == 0x0a) | (xch ==0x3e) )  {
+
 				*bptr = 0x00;
 				iResult =1;
 				fEnding = 1;
@@ -255,22 +198,6 @@ int8_t *bptr, xch;
 	}
 
 	return iResult;
-}*/
-/**************/
-
-//************************************************************/
-/*****************************/
-/**** Close COM Port     *****/
-/*****************************/
-void _ClosePort(void)
-{
-	//pthread_cancel(InQueueID);	
-	//pthread_mutex_destroy(&buf_mut);
-	//pthread_mutex_destroy(&port_mut);
-	tcsetattr(fcom, TCSANOW, &old_tios);	//restore setting
-	close(fcom);
-	dwBaudrate = 0;
-	fcom = 0;
 }
 /*****************************/
 /**** Open COM Port      *****/
@@ -286,86 +213,22 @@ int32_t iret;
 	//fcom = open("/dev/ttyUSB0", O_RDWR | O_NDELAY);
 fcom = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
 	if (fcom < 0) return 0;
-	//tcgetattr(fcom, &old_tios);		//backup setting
-//printf("open pass");
+	//else printf("fcom=%x",fcom);
 	//Buffer pointer initial
-	//wRxCounter =0 ;	
-	//pWritePtr = &RxBuffer[0];
-	//pReadPtr = &RxBuffer[0];
-	//pRxBufferStart = &RxBuffer[0];
-	//pRxBufferEnd = &RxBuffer[BUFFER_SIZE-1];
+	wRxCounter =0 ;	
+	pWritePtr = &RxBuffer[0];
+	pReadPtr = &RxBuffer[0];
+	pRxBufferStart = &RxBuffer[0];
+	pRxBufferEnd = &RxBuffer[BUFFER_SIZE-1];
 	//hook receiver thread
 	//fcom=1;
-	//pthread_mutex_init(&port_mut, NULL);
-	//pthread_mutex_init(&buf_mut, NULL);
-	//pthread_create(&InQueueID, (pthread_attr_t*)(0), _IncomeInQueueThread, (void*)(0));
+	pthread_mutex_init(&buf_mut, NULL);
+	pthread_create(&InQueueID, (pthread_attr_t*)(0), _IncomeInQueueThread, (void*)(0));
 
-	//set_interface_attribs(fcom, B115200, 0); 
-
-
-
-tcgetattr(fcom, &SerialPortSettings);
-
-        cfsetospeed (&SerialPortSettings, B115200);
-        cfsetispeed (&SerialPortSettings, B115200);
-		/* 8N1 Mode */
-		SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
-		SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
-		SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
-		SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
-		
-		SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
-		SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
-		
-		
-		SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
-		SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
-
-		SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
-       /* SerialPortSettings.c_cflag = (SerialPortSettings.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        //SerialPortSettings.c_iflag &= ~IGNBRK;         // disable break processing
-        //SerialPortSettings.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        SerialPortSettings.c_oflag = 0;                // no remapping, no delays
-        SerialPortSettings.c_cc[VMIN]  = 0;            // read doesn't block
-        SerialPortSettings.c_cc[VTIME] = 0;            // 0.5 seconds read timeout
-
-        SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-        SerialPortSettings.c_cflag |= (CLOCAL | CREAD);	// ignore modem controls,
-                                        	// enable reading
-        SerialPortSettings.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        SerialPortSettings.c_cflag |= parity;
-        SerialPortSettings.c_cflag &= ~CSTOPB;
-        SerialPortSettings.c_cflag &= ~CRTSCTS;*/
-printf("c flag = %X\n",SerialPortSettings.c_cflag);
-printf("i flag = %X\n",SerialPortSettings.c_iflag);
-printf("o flag = %X\n",SerialPortSettings.c_oflag);
-printf("l flag = %X\n",SerialPortSettings.c_lflag);
-        if (tcsetattr (fcom, TCSANOW, &SerialPortSettings) != 0)
-        {
-                printf("error %d from tcsetattr", errno);
-                return -1;
-        }
+	set_interface_attribs(B115200); 
 
 
 
-
-
- char *wrCmd = "I";
-
-int rcvCnt=0;
-	iResult = write(fcom,"I", 1);
-printf("SendCnt = %d, data=%x\n",iResult,*wrCmd);
-rcvCnt = read(fcom, rdData, 100);
-//for(int c=0;c<rcvCnt;c++)
-//{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[0]);
-//}
-		//not checking device when assign baudrate
-		//iret = _ReadBufferLength(rdData, 2);
-		//if ( rdData[0]==0x10 && rdData[1]==0x02 )iResult = 1;
 	iResult = 1;
 	
 	
@@ -376,96 +239,119 @@ printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[0]);
 int main(int argc, char **argv) 
 {
 int iResult = 0;
-	//int fcom=0;
-char *wrCmd = "I";
-	char *com_path = "/dev/ttyUSB0";
+char *com_path = "/dev/ttyUSB0";
 
 char rdData[100];
 int rcvCnt=0;
 
 	if(argc<2)
 	{
-		printf("./AD411_util -continue\n");
-		printf("./AD411_util -single\n");
-		printf("./AD411_util -identify\n");
-		printf("./AD411_util -single\n");
-		printf("./AD411_util -gain\n");
-		printf("./AD411_util -offset\n");
-		printf("./AD411_util -save\n");
+		printf("./AD4111_util -continue\n");
+		printf("./AD4111_util -single\n");
+		printf("./AD4111_util -identify\n");
+		printf("./AD4111_util -single\n");
+		printf("./AD4111_util -select [channel]\n");
+		printf("./AD4111_util -gain\n");
+		printf("./AD4111_util -offset\n");
+		printf("./AD4111_util -save\n");
 
-return 0;
+		return 0;
 	}
-
-	if(strcmp("-continue",argv[1])==0)
-	{printf("1\n");}
-	else if(strcmp("-single",argv[1])==0)
-	{printf("2\n");}
-	else if(strcmp("-identify",argv[1])==0)
+	iResult = _OpenPort(com_path);
+	if(iResult == 1)
 	{
-		iResult = _OpenPort(com_path);
-		if(iResult == 1)
+		if(strcmp("-continue",argv[1])==0)
+		{printf("1\n");}
+		else if(strcmp("-single",argv[1])==0)
 		{
-		printf("open done\n");
-			iResult = _SendBufferLength(wrCmd,1);
-
+			iResult = _SendBufferLength("S",1);
 			if(iResult == 1)
-			{printf("send success\n");
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-rcvCnt = read(fcom, rdData, 100);
-for(int c=0;c<rcvCnt;c++)
-{
-printf("rcvCnt = %d, data=%c\n",rcvCnt,rdData[c]);
-}
-				//iResult = _ReadBufferLength(rdData, 20000);
-				//if ( iResult ) {
-				//		printf("receive data");
-				//}	
+			{
+				printf("send success\n");
+				usleep(10000);
+				iResult = _ReadBuffer(rdData);
+				if(iResult == 1)
+				{
+					printf("read success\n");
+					printf("%s\n",rdData);
+					usleep(50000);
+				}
+				iResult = _ReadBuffer(rdData);
+				if(iResult == 1)
+				{
+					printf("read success\n");
+					printf("%s\n",rdData);
+					usleep(50000);
+				}
+				iResult = _ReadBuffer(rdData);
+				if(iResult == 1)
+				{
+					printf("read success\n");
+					printf("%s\n",rdData);
+					usleep(50000);
+				}
 			}
 		}
+		else if(strcmp("-identify",argv[1])==0)
+		{
+			
+			iResult = _SendBufferLength("I",1);
+			if(iResult == 1)
+			{
+				//printf("send success\n");
+				
+				usleep(50000);
+				iResult = _ReadBuffer(rdData);
+				if(iResult == 1)
+				{
+					printf("read success\n");
+					printf("%s\n",rdData);
+					usleep(50000);
+				}
+				iResult = _ReadBuffer(rdData);
+				if(iResult == 1)
+				{
+					printf("read success\n");
+					printf("%s\n",rdData);
+					usleep(50000);
+				}
+				iResult = _ReadBuffer(rdData);
+				if(iResult == 1)
+				{
+					printf("read success\n");
+					printf("%s\n",rdData);
+					usleep(50000);
+				}
+
+			}
+		}			
+		else if(strcmp("-select",argv[1])==0)
+		{
+			iResult = _SendBufferLength("M",1);
+			if(iResult == 1)
+			{
+				printf("send success\n");
+				usleep(100000);
+				//*wrCmd = atoi(argv[2]);
+				iResult = _SendBufferLength(argv[2],1);
+				if(iResult == 1)
+				{
+					printf("send success\n");
+					usleep(1000000);
+				}
+			}
+
+		}
+		else if(strcmp("-gain",argv[1])==0)
+		{printf("4\n");}
+		else if(strcmp("-offset",argv[1])==0)
+		{printf("5\n");}
+		else if(strcmp("-save",argv[1])==0)
+		{printf("6\n");}
 		else
-		{	
-			printf("fail to open : %s\n",com_path);
-		}		//wrCmd[1] =;
+		{printf("7\n");}
+
 	}
-	else if(strcmp("-gain",argv[1])==0)
-	{printf("4\n");}
-	else if(strcmp("-offset",argv[1])==0)
-	{printf("5\n");}
-	else if(strcmp("-save",argv[1])==0)
-	{printf("6\n");}
-	else
-	{printf("7\n");}
-
-
 close(fcom);
 
 return 0;
